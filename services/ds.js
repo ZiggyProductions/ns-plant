0591925d2ee70b6317e86cd7714fc6e2dbdae236
Gevorg Aznauryan <gazilius@gmail.com>
2017-07-27 15:02:09 +0300

    ds only same host spots

--- services/ds.js
+++ services/ds.js
@@ -2,6 +2,7 @@
  * Created by GAZ on 7/26/17.
  */
 var Player = require('../models/player');
+var os = require('os');
 //var DeepstreamServer = require('deepstream.io');
 var DeepstreamClient = require('deepstream.io-client-js');
 
@@ -35,13 +36,22 @@ module.exports = {
             //     console.error(err);
             // }
             try {
-                const endpoints = _.map(_.filter(ns.clients, function (c) {
-                    return c.connected && c.meta.node == 'ns-cell';
+
+                var net = os.networkInterfaces();
+                var myip;
+                _.each(net,function(ifaces){
+                    _.each(ifaces,function (iface) {
+                        if(iface.internal == false && iface.family == 'IPv4')
+                            myip = iface.address;
+                    })
+                })
+
+                const endpoint = _.first(_.shuffle(_.map(_.filter(ns.clients, function (c) {
+                    return c.connected && c.meta.node == 'ns-cell' && c.meta.ds.ip == myip;
                 }), function (n) {
                     return n.meta.ds;
-                });
-                const endpoint = _.first(_.shuffle(endpoints));
-                this.client = DeepstreamClient(endpoint.ip + ':' + endpoint.port).login({user: 'plant-client'});
+                })));
+                this.client = DeepstreamClient(/*endpoint.ip + */'localhost:' + endpoint.port).login({user: 'plant-client'});
             }catch(err){
                 console.error(err);
             }
@@ -50,6 +60,8 @@ module.exports = {
 
         addPlayer: function(p){
             try {
+                if(!this.client)
+                    this.init();
                 const player = this.client.record.getRecord('players/'+p.id);
                 player.set(p);
             }catch(err){
